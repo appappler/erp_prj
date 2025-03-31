89ad4b344cbab457831b7a4537968f655f41a512
user <user@DESKTOP-VAIMDE6>
2025-03-31 14:33:00 +0900

    첨부 링크 올립니다.

--- erp_prj/src/kr/co/sist/csj/Csj.java
+++ erp_prj/src/kr/co/sist/csj/Csj.java
@@ -3,9 +3,7 @@ package kr.co.sist.csj;
 public class Csj {
 
 	public static void main(String[] args) {
-		System.out.println("빌스택스 TNF");
-		System.out.println("빌스택스 420FLOW");
-		System.out.println("랍온어비트 YOUNG BOY");
+		System.out.println("https://www.youtube.com/watch?v=zgNGO6Xiego");
 	}
 
 }
