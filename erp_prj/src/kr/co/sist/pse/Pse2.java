484f489759f49e9ae874cbeb276d448e320b357b
user <user@DESKTOP-JK98RUC>
2025-03-31 14:17:14 +0900

    안녕하세요^^

---
+++ erp_prj/src/kr/co/sist/pse/Pse2.java
@@ -0,0 +1,9 @@
+package kr.co.sist.pse;
+
+public class Pse2 {
+
+	public static void main(String[] args) {
+		System.out.println("자신이 구현해야할 코드 작성");
+		System.out.println("PSE");
+	}//main
+}//class
