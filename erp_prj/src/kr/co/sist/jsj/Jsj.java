08e9a26e9694a835bd8dc1dcd9e3b75205309a09
user <user@DESKTOP-8TALBT7>
2025-03-31 15:44:18 +0900

    커밋

--- erp_prj/src/kr/co/sist/jsj/Jsj.java
+++ erp_prj/src/kr/co/sist/jsj/Jsj.java
@@ -4,6 +4,6 @@ public class Jsj {
 	public static void main(String[] args) {
 		System.out.println("두번째입니다");
 		System.out.println("jsj 생성");
-		System.out.println("jsj 생성");
+		System.out.println("커밋");
 	}
 }
