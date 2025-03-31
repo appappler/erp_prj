a249d71c2b87f628b868bc109fd95d96a6f80afc
user <user@DESKTOP-JK98RUC>
2025-03-31 12:39:17 +0900

    박선은이 했습니다.

---
+++ erp_prj/src/kr/co/sist/pse/Pse.java
@@ -0,0 +1,9 @@
+package kr.co.sist.pse;
+
+public class Pse {
+
+	public static void main(String[] args) {
+		System.out.println("자신이 구현해야할 코드 작성");
+		System.out.println("PSE");
+	}//main
+}//class
