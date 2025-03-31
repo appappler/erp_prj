1617b95095b2b6233d80bee3d7a9fdc0990b0a1d
user <user@localhost>
2025-03-31 12:40:15 +0900

    이게머지

---
+++ erp_prj/src/kr/co/sist/kmj/Kmj.java
@@ -0,0 +1,11 @@
+package kr.co.sist.kmj;
+
+public class Kmj {
+
+	public static void main(String[] args) {
+		System.out.println("내가 만든 쿠키...");
+		System.out.println("너를 위해 구웠지!");
+		
+	}//main
+
+}//class
