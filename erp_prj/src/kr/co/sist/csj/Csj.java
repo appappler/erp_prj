9ad869abae5a97b2b718a3b6f8cb437b91bae83a
user <user@DESKTOP-VAIMDE6>
2025-03-31 14:28:07 +0900

    뭘봐 ㅡㅡ

---
+++ erp_prj/src/kr/co/sist/csj/Csj.java
@@ -0,0 +1,11 @@
+package kr.co.sist.csj;
+
+public class Csj {
+
+	public static void main(String[] args) {
+		System.out.println("빌스택스 TNF");
+		System.out.println("빌스택스 420FLOW");
+		System.out.println("랍온어비트 YOUNG BOY");
+	}
+
+}
