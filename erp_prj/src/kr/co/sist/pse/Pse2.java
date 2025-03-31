48340f01e01bd56b6fc315475f82b9e923cf8612
user <user@DESKTOP-JK98RUC>
2025-03-31 14:49:34 +0900

    테스트를 위한 클래스예요

--- erp_prj/src/kr/co/sist/pse/Pse2.java
+++ erp_prj/src/kr/co/sist/pse/Pse2.java
@@ -3,7 +3,6 @@ package kr.co.sist.pse;
 public class Pse2 {
 
 	public static void main(String[] args) {
-		System.out.println("자신이 구현해야할 코드 작성");
-		System.out.println("PSE");
+		System.out.println("테스트");
 	}//main
 }//class
