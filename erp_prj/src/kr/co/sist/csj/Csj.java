012202675faf718adb3e8c7d8d0d70f8b076c1f8
user <user@DESKTOP-VAIMDE6>
2025-04-01 17:51:47 +0900

    0401

--- erp_prj/src/kr/co/sist/csj/Csj.java
+++ erp_prj/src/kr/co/sist/csj/Csj.java
@@ -4,7 +4,7 @@ public class Csj {
 
 	public static void main(String[] args) {
 		System.out.println("https://www.youtube.com/watch?v=zgNGO6Xiego");
-		System.out.println("내방에서 나가");
+		System.out.println("내방에서 나가!");
 	}
 
 }
