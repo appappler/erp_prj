6648d865926771520f8b5a750753becc9dd97412
user <user@DESKTOP-VAIMDE6>
2025-04-03 16:10:04 +0900

    4시 10분입니다.

--- erp_prj/src/kr/co/sist/csj/Csj.java
+++ erp_prj/src/kr/co/sist/csj/Csj.java
@@ -4,11 +4,11 @@ public class Csj {
 
 	public static void main(String[] args) {
 		System.out.println("https://www.youtube.com/watch?v=zgNGO6Xiego");
-<<<<<<< HEAD
+
 		System.out.println("내방에서 나가!");
-=======
-		System.out.println("4444444월3일");
->>>>>>> refs/heads/csj
+
+		System.out.println("4시9분입니다.");
+
 	}
 
 }
